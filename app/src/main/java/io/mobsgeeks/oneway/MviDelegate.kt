cc9039c45cddb155ddb26a3d09642252a478679e
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-07-02 22:50:29 +0530

    MviDelegate, dispose subscription on teardown.

--- app/src/main/java/io/mobsgeeks/oneway/MviDelegate.kt
+++ app/src/main/java/io/mobsgeeks/oneway/MviDelegate.kt
@@ -4,10 +4,17 @@ import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 
 class MviDelegate<T> {
+  private lateinit var disposable: Disposable
+
   fun setup(
       sourceFunction: () -> Observable<T>,
       sinkFunction: (Observable<T>) -> Disposable
   ): Disposable { // TODO(rj) 2/Jul/18 - Ensure this disposable can't be unsubscribed.
-    return sinkFunction(sourceFunction())
+    disposable = sinkFunction(sourceFunction())
+    return disposable
+  }
+
+  fun teardown() {
+    disposable.dispose()
   }
 }
--- app/src/test/java/io/mobsgeeks/oneway/MviDelegateTest.kt
+++ app/src/test/java/io/mobsgeeks/oneway/MviDelegateTest.kt
@@ -4,6 +4,7 @@ import com.google.common.truth.Truth.assertThat
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import io.reactivex.observers.TestObserver
+import io.reactivex.subjects.PublishSubject
 import org.junit.Test
 
 class MviDelegateTest {
@@ -24,5 +25,28 @@ class MviDelegateTest {
 
     assertThat(disposable.isDisposed)
         .isFalse()
+  }
+
+  @Test fun `it disposes the subscription on teardown`() {
+    // given
+    val publisher = PublishSubject.create<String>()
+    val sourceFunction = { publisher }
+    val testObserver = TestObserver<String>()
+    val sinkFunction: (Observable<String>) -> Disposable = { it -> it.subscribeWith(testObserver) }
+    val mviDelegate = MviDelegate<String>()
+
+    // when
+    val disposable = mviDelegate.setup(sourceFunction, sinkFunction)
+    val firstMessage = "Hello"
+    publisher.onNext(firstMessage)
+    mviDelegate.teardown()
+    publisher.onNext("Unreachable")
+
+    // then
+    testObserver.assertNoErrors()
+    testObserver.assertValue(firstMessage)
+
+    assertThat(disposable.isDisposed)
+        .isTrue()
   }
 }
