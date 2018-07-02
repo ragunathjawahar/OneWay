37353d865286c8bcbcfe415878363406dd8a2e02
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-07-02 22:10:10 +0530

    Just one way of doing it.

---
+++ app/src/test/java/io/mobsgeeks/oneway/CanaryUnitTest.kt
@@ -0,0 +1,10 @@
+package io.mobsgeeks.oneway
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CanaryUnitTest {
+  @Test fun `test environment is setup`() {
+    assertEquals(4, 2 + 2)
+  }
+}
