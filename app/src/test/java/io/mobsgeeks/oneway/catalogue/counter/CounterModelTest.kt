e8b97db09362475aaf0f93a6475df6afd3237bd1
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-07-22 21:14:22 +0530

    CounterModelTest, rewrite using 'core-test'.

--- app/src/test/java/io/mobsgeeks/oneway/catalogue/counter/CounterModelTest.kt
+++ app/src/test/java/io/mobsgeeks/oneway/catalogue/counter/CounterModelTest.kt
@@ -1,73 +1,53 @@
 package io.mobsgeeks.oneway.catalogue.counter
 
-import io.mobsgeeks.oneway.Binding
-import io.mobsgeeks.oneway.Binding.CREATED
-import io.mobsgeeks.oneway.Binding.RESTORED
 import io.mobsgeeks.oneway.catalogue.counter.CounterState.Companion.ZERO
 import io.mobsgeeks.oneway.catalogue.counter.usecases.*
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.observers.TestObserver
+import io.mobsgeeks.oneway.test.MviTestRule
 import io.reactivex.subjects.PublishSubject
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
 
 class CounterModelTest {
   private val intentions = PublishSubject.create<CounterIntention>()
-  private val bindings = PublishSubject.create<Binding>()
-  private val timeline = PublishSubject.create<CounterState>()
-  private val testObserver = TestObserver<CounterState>()
-  private val disposable: CompositeDisposable = CompositeDisposable()
 
-  private val useCases = CounterUseCases(
-      CreatedUseCase(),
-      RestoredUseCase(timeline),
-      IncrementUseCase(timeline),
-      DecrementUseCase(timeline)
-  )
-
-  @Before fun setup() {
-    val sharedStates = CounterModel
-        .bind(intentions, bindings, useCases)
-        .share()
-    disposable.addAll(
-        sharedStates.subscribe { timeline.onNext(it) },
-        sharedStates.subscribeWith(testObserver)
+  private val mviTestRule = MviTestRule<CounterState> { bindings, timeline ->
+    val useCases = CounterUseCases(
+        CreatedUseCase(),
+        RestoredUseCase(timeline),
+        IncrementUseCase(timeline),
+        DecrementUseCase(timeline)
     )
-  }
 
-  @After fun teardown() {
-    disposable.dispose()
+    return@MviTestRule CounterModel.bind(intentions, bindings, useCases)
   }
 
   @Test fun `creating the screen starts with a ZERO state`() {
     // when
-    screenIsCreated()
+    mviTestRule.screenIsCreated()
 
     // then
-    assertStates(testObserver, ZERO)
+    mviTestRule.assertStates(ZERO)
   }
 
   @Test fun `tapping on + increments the counter by 1`() {
     // when
-    startWith(ZERO) {
+    mviTestRule.startWith(ZERO) {
       increment()
     }
 
     // then
     val one = ZERO.add(1)
-    assertStates(testObserver, one)
+    mviTestRule.assertStates(one)
   }
 
   @Test fun `tapping on - decrements the counter by 1`() {
     // when
-    startWith(ZERO) {
+    mviTestRule.startWith(ZERO) {
       decrement()
     }
 
     // then
     val minusOne = ZERO.add(-1)
-    assertStates(testObserver, minusOne)
+    mviTestRule.assertStates(minusOne)
   }
 
   @Test fun `restoring the screen preserves the previous state`() {
@@ -75,19 +55,12 @@ class CounterModelTest {
     val three = CounterState(3)
 
     // when
-    startWith(three) {
-      screenIsRestored()
+    mviTestRule.startWith(three) {
+      mviTestRule.screenIsRestored()
     }
 
     // then
-    with(testObserver) {
-      assertNoErrors()
-      assertValues(three)
-    }
-  }
-
-  private fun screenIsCreated() {
-    bindings.onNext(CREATED)
+    mviTestRule.assertStates(three)
   }
 
   private fun increment() {
@@ -96,28 +69,5 @@ class CounterModelTest {
 
   private fun decrement() {
     intentions.onNext(Decrement)
-  }
-
-  private fun screenIsRestored() {
-    bindings.onNext(RESTORED)
-  }
-
-  private fun assertStates(
-      testObserver: TestObserver<CounterState>,
-      vararg states: CounterState
-  ) {
-    with(testObserver) {
-      assertNoErrors()
-      assertValues(*states)
-      assertNotTerminated()
-    }
-  }
-
-  private fun startWith(
-      startState: CounterState,
-      block: () -> Unit
-  ) {
-    timeline.onNext(startState)
-    block()
   }
 }
