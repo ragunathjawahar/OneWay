8534823ddbe4a3ee0b902d58d5013e830f80d3c1
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-07-31 06:49:09 +0530

    MviActivity, extract base activity.

--- app/src/main/java/io/mobsgeeks/oneway/catalogue/counter/CounterActivity.kt
+++ app/src/main/java/io/mobsgeeks/oneway/catalogue/counter/CounterActivity.kt
@@ -1,28 +1,21 @@
 package io.mobsgeeks.oneway.catalogue.counter
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.jakewharton.rxbinding2.view.clicks
 import io.mobsgeeks.oneway.Binding
-import io.mobsgeeks.oneway.MviDelegate
-import io.mobsgeeks.oneway.Sink
-import io.mobsgeeks.oneway.Source
 import io.mobsgeeks.oneway.catalogue.R
 import io.mobsgeeks.oneway.catalogue.counter.drivers.CounterViewDriver
 import io.mobsgeeks.oneway.catalogue.counter.usecases.CounterUseCases
 import io.mobsgeeks.oneway.catalogue.counter.usecases.DecrementUseCase
 import io.mobsgeeks.oneway.catalogue.counter.usecases.IncrementUseCase
+import io.mobsgeeks.oneway.catalogue.mvi.MviActivity
 import io.mobsgeeks.oneway.usecases.DefaultBindingCreatedUseCase
 import io.mobsgeeks.oneway.usecases.DefaultBindingRestoredUseCase
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.counter_fragment.*
 
-private const val KEY_STATE = "state"
-
-class CounterActivity : AppCompatActivity(), CounterView {
-  private val mviDelegate: MviDelegate<CounterState, CounterState> = MviDelegate()
-
+class CounterActivity : MviActivity<CounterState>(), CounterView {
   private val intentions: CounterIntentions
     get() = CounterIntentions(incrementButton.clicks(), decrementButton.clicks())
 
@@ -30,13 +23,13 @@ class CounterActivity : AppCompatActivity(), CounterView {
     get() = DefaultBindingCreatedUseCase(CounterState.ZERO)
 
   private val restoredUseCase: DefaultBindingRestoredUseCase<CounterState>
-    get() = DefaultBindingRestoredUseCase(mviDelegate.timeline)
+    get() = DefaultBindingRestoredUseCase(timeline)
 
   private val incrementUseCase: IncrementUseCase
-    get() = IncrementUseCase(mviDelegate.timeline)
+    get() = IncrementUseCase(timeline)
 
   private val decrementUseCase: DecrementUseCase
-    get() = DecrementUseCase(mviDelegate.timeline)
+    get() = DecrementUseCase(timeline)
 
   private val useCases: CounterUseCases
     get() = CounterUseCases(
@@ -56,37 +49,14 @@ class CounterActivity : AppCompatActivity(), CounterView {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.counter_fragment)
-
-    val state = savedInstanceState?.get(KEY_STATE) as CounterState?
-    mviDelegate.restoreState(state)
   }
 
-  override fun onStart() {
-    super.onStart()
-    val source: Source<CounterState> = object : Source<CounterState> {
-      override fun produce(
-          bindings: Observable<Binding>,
-          timeline: Observable<CounterState>
-      ): Observable<CounterState> =
-          CounterModel.bind(intentions.stream(), bindings, useCases)
-    }
+  override fun source(
+      bindings: Observable<Binding>,
+      timeline: Observable<CounterState>
+  ): Observable<CounterState> =
+      CounterModel.bind(intentions.stream(), bindings, useCases)
 
-    val sink: Sink<CounterState> = object : Sink<CounterState> {
-      override fun consume(source: Observable<CounterState>): Disposable =
-          viewDriver.render(source)
-    }
-
-    mviDelegate.bind(source, sink)
-  }
-
-  override fun onStop() {
-    mviDelegate.unbind()
-    super.onStop()
-  }
-
-  override fun onSaveInstanceState(outState: Bundle) {
-    val state = mviDelegate.saveState()
-    outState.putParcelable(KEY_STATE, state)
-    super.onSaveInstanceState(outState)
-  }
+  override fun sink(source: Observable<CounterState>): Disposable =
+      viewDriver.render(source)
 }
