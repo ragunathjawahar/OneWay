2fb00779b18e52da206c7487f35f89b012ce9541
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-08-01 22:36:05 +0530

    BMI example, implement BmiView in BmiActivity.

--- app/src/main/java/io/mobsgeeks/oneway/catalogue/bmi/BmiActivity.kt
+++ app/src/main/java/io/mobsgeeks/oneway/catalogue/bmi/BmiActivity.kt
@@ -1,10 +1,14 @@
 package io.mobsgeeks.oneway.catalogue.bmi
 
 import android.os.Bundle
+import android.support.annotation.StringRes
 import com.jakewharton.rxbinding2.widget.changes
 import com.jakewharton.rxbinding2.widget.checkedChanges
 import io.mobsgeeks.oneway.Binding
 import io.mobsgeeks.oneway.catalogue.R
+import io.mobsgeeks.oneway.catalogue.bmi.MeasurementSystem.IMPERIAL
+import io.mobsgeeks.oneway.catalogue.bmi.MeasurementSystem.SI
+import io.mobsgeeks.oneway.catalogue.bmi.drivers.BmiViewDriver
 import io.mobsgeeks.oneway.catalogue.bmi.usecases.BmiUseCases
 import io.mobsgeeks.oneway.catalogue.bmi.usecases.ChangeHeightUseCase
 import io.mobsgeeks.oneway.catalogue.bmi.usecases.ChangeMeasurementSystemUseCase
@@ -16,7 +20,7 @@ import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.bmi_fragment.*
 
-class BmiActivity : MviActivity<BmiState>() {
+class BmiActivity : MviActivity<BmiState>(), BmiView {
   private val intentions
     get() = BmiIntentions(
         weightSeekBar.changes().skipInitialValue(),
@@ -33,6 +37,9 @@ class BmiActivity : MviActivity<BmiState>() {
         ChangeMeasurementSystemUseCase(timeline)
     )
 
+  private val viewDriver
+    get() = BmiViewDriver(this)
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.bmi_fragment)
@@ -44,7 +51,38 @@ class BmiActivity : MviActivity<BmiState>() {
   ): Observable<BmiState> =
     BmiModel.bind(intentions.stream(), bindings, useCases)
 
-  override fun sink(source: Observable<BmiState>): Disposable {
-    TODO("not implemented")
+  override fun sink(source: Observable<BmiState>): Disposable =
+      viewDriver.render(source)
+
+  override fun showBmi(bmi: Double) {
+    bmiTextView.text = getString(R.string.template_bmi, bmi)
+  }
+
+  override fun showCategory(category: BmiCategory) {
+    bmiCategoryTextView.text = category.toString()
+  }
+
+  override fun showWeight(weight: Double, measurementSystem: MeasurementSystem) {
+    @StringRes val weightTemplateStringRes = when (measurementSystem) {
+      SI       -> R.string.template_weight_si
+      IMPERIAL -> R.string.template_weight_imperial
+    }
+    weightTextView.text = getString(weightTemplateStringRes, weight)
+  }
+
+  override fun showHeight(height: Double, measurementSystem: MeasurementSystem) {
+    @StringRes val heightTemplateStringRes = when (measurementSystem) {
+      SI       -> R.string.template_height_si
+      IMPERIAL -> R.string.template_height_imperial
+    }
+    heightTextView.text = getString(heightTemplateStringRes, height)
+  }
+
+  override fun showMeasurementSystem(measurementSystem: MeasurementSystem) {
+    @StringRes val measurementSystemRes = when (measurementSystem) {
+      SI       -> R.string.si_units
+      IMPERIAL -> R.string.imperial_units
+    }
+    measurementSystemSwitch.text = getString(measurementSystemRes)
   }
 }
--- app/src/main/java/io/mobsgeeks/oneway/catalogue/bmi/BmiView.kt
+++ app/src/main/java/io/mobsgeeks/oneway/catalogue/bmi/BmiView.kt
@@ -4,6 +4,6 @@ interface BmiView {
   fun showBmi(bmi: Double)
   fun showCategory(category: BmiCategory)
   fun showWeight(weight: Double, measurementSystem: MeasurementSystem)
-  fun showHeight(weight: Double, measurementSystem: MeasurementSystem)
+  fun showHeight(height: Double, measurementSystem: MeasurementSystem)
   fun showMeasurementSystem(measurementSystem: MeasurementSystem)
 }
