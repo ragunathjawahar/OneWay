09edf369fc7ae1a62b69358e5392f40eea800a8d
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-08-18 21:00:05 +0530

    CanaryTest, migrate to JUnit 5.

--- app/src/test/java/io/redgreen/oneway/catalogue/CanaryTest.kt
+++ app/src/test/java/io/redgreen/oneway/catalogue/CanaryTest.kt
@@ -1,7 +1,7 @@
 package io.redgreen.oneway.catalogue
 
 import com.google.common.truth.Truth.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 class CanaryTest {
   @Test fun `test environment is setup`() {
