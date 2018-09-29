a9457f2ea1454d32fcc8b450db40c8edde87042e
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-09-30 01:22:57 +0530

    TheActivity, DexFile example.

--- app/src/main/java/io/redgreen/oneway/catalogue/TheActivity.kt
+++ app/src/main/java/io/redgreen/oneway/catalogue/TheActivity.kt
@@ -2,6 +2,8 @@ package io.redgreen.oneway.catalogue
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import dalvik.system.DexFile
+import io.redgreen.oneway.android.OneWayFragment
 import io.redgreen.oneway.catalogue.showcase.ShowcaseFragment
 
 class TheActivity : AppCompatActivity() {
@@ -15,5 +17,19 @@ class TheActivity : AppCompatActivity() {
           .replace(R.id.container, ShowcaseFragment())
           .commit()
     }
+
+    DexFile(packageCodePath)
+        .entries()
+        .toList()
+        .forEach {
+          try {
+            val clazz = Class.forName(it)
+            if (OneWayFragment::class.java.isAssignableFrom(clazz)) {
+              println(it)
+            }
+          } catch (e: Throwable) {
+            println("$it is pecked up!")
+          }
+        }
   }
 }
