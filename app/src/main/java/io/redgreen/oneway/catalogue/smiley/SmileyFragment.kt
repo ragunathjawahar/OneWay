9cc376c92fd9784cd68f720e70125774f26792d6
Ragunath Jawahar <ragunathjawahar@gmail.com>
2018-10-23 06:41:59 +0530

    SmileyFragment, use ResultDispatcher to handle Activity results.

--- app/src/main/java/io/redgreen/oneway/catalogue/smiley/SmileyFragment.kt
+++ app/src/main/java/io/redgreen/oneway/catalogue/smiley/SmileyFragment.kt
@@ -8,10 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.subjects.PublishSubject
 import io.redgreen.oneway.SourceLifecycleEvent
 import io.redgreen.oneway.android.OneWayFragment
+import io.redgreen.oneway.android.ResultDispatcher
 import io.redgreen.oneway.catalogue.R
 import io.redgreen.oneway.catalogue.smiley.SmileyPickerActivity.Companion.REQUEST_PICK_SMILEY
 import io.redgreen.oneway.catalogue.smiley.usecases.SmileyUseCases
@@ -23,7 +26,8 @@ class SmileyFragment : OneWayFragment<SmileyState>() {
   private val initialState: SmileyState
     get() = SmileyState.initial(getString(R.string.emoji_thumbs_up))
 
-  private var pickedSmiley: String? = null
+  private val resultDispatcher = ResultDispatcher()
+  private val compositeDisposable = CompositeDisposable()
 
   override fun onCreateView(
       inflater: LayoutInflater,
@@ -57,21 +61,25 @@ class SmileyFragment : OneWayFragment<SmileyState>() {
 
   override fun onStart() {
     super.onStart()
-    pickedSmiley?.let {
-      intentions.onNext(PickSmileyIntention(it))
-      pickedSmiley = null
-    }
+    compositeDisposable += resultDispatcher
+        .result<String>()
+        .map { PickSmileyIntention(it) }
+        .subscribe { intentions.onNext(it) }
+  }
+
+  override fun onStop() {
+    super.onStop()
+    compositeDisposable.clear()
   }
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-
-    val pickSmileySuccessful = requestCode == REQUEST_PICK_SMILEY
-        && resultCode == RESULT_OK
-        && data != null
-
-    if (pickSmileySuccessful) {
-      pickedSmiley = SmileyPickerActivity.getSmiley(data!!)
+    val isPickSmileyResult = requestCode == REQUEST_PICK_SMILEY
+    if (isPickSmileyResult) {
+      if (resultCode == RESULT_OK) {
+        resultDispatcher.setResult(SmileyPickerActivity.getSmiley(data!!))
+      } else {
+        // TODO(rj) 23/Oct/18 - Handle cancelled results.
+      }
     }
   }
 }
